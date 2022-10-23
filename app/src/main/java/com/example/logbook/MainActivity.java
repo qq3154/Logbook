49bb4452df246ba299496ddfef2ef7073c0e57d2
QQ3154 <kjbinben@gmail.com>
2022-10-23 21:24:04 +0700

    finish logbook 2

--- app/src/main/java/com/example/logbook/MainActivity.java
+++ app/src/main/java/com/example/logbook/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.logbook;
 
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.URLUtil;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imgView;
     private Button btnPrevious;
     private Button btnForward;
+    private Button btnAdd;
+    private EditText edtUrl;
 
     List<String> imageUrls;
     int currentImage;
@@ -33,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
         imgView = findViewById(R.id.img_View);
         btnPrevious = findViewById(R.id.btn_previous);
         btnForward = findViewById(R.id.btn_forward);
+        btnAdd = findViewById(R.id.btn_add);
+        edtUrl = findViewById(R.id.edt_url);
 
         btnPrevious.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,12 +55,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        insertImage();
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addImage();
+            }
+        });
+
+        insertImageOnStart();
 
         loadData();
     }
 
-    private void insertImage(){
+    private void insertImageOnStart(){
         imageUrls = new ArrayList<>();
         currentImage = 0;
         maxImage = 3;
@@ -62,9 +76,33 @@ public class MainActivity extends AppCompatActivity {
         imageUrls.add("https://www.w3schools.com/w3css/img_lights.jpg");
     }
 
+    private void addImage() {
+        String url = edtUrl.getText().toString().trim();
+
+        if(url == null || url.length() == 0){
+            Toast.makeText(this, "Please enter URL!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(!URLUtil.isValidUrl(url)){
+            Toast.makeText(this, "Invalid URL!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //we can add image here
+        imageUrls.add(url);
+
+        currentImage = maxImage;
+
+        loadData();
+
+        edtUrl.setText("");
+
+    }
 
 
     private void loadData(){
+        maxImage = imageUrls.size();
 
         if(currentImage < maxImage){
             loadImage(imageUrls.get(currentImage));
